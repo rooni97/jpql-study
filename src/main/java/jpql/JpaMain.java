2a6d84f013a431fd1548aebb7cffb95eb7d19b62
cy <cy@cyui-MacBookAir.local>
2022-09-16 12:04:02 +0900

    initial commit

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,75 @@
+package jpql;
+
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class JpaMain {
+
+    public static void main(String[] arg) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(0);
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(0);
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(0);
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+//            int resultCount = em.createQuery("update Member m set m.age = 20")
+//                    .executeUpdate();
+
+            List<Member> members = em.createQuery("select m from Member m where m.team in :team", Member.class)
+                    .setParameter("team", List.of(teamA, teamB))
+                    .getResultList();
+
+            for (Member member : members) {
+                System.out.println("member = " + member.getUsername());
+            }
+
+            em.clear();
+//            Member findMember = em.find(Member.class, member1.getId());
+//            System.out.println("findMember = " + findMember.getAge());
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
